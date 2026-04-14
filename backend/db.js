d01e92767d915a58aa2304149514288ee95c4c95
KumarShrey <kumarshrey14477@gmail.com>
2026-04-14 20:23:23 +0530

    Fix Bug

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,9 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./.env" });
+dotenv.config();
 
-const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const db = await mysql.createConnection(process.env.MYSQL_URL);
 
 console.log("MySQL Connected");
 
