117194957cc52cf96203df5f2496599fc8f2308f
KumarShrey <kumarshrey14477@gmail.com>
2026-04-15 20:50:30 +0530

    Profile Logo Bug Fix

--- app/profile/page.js
+++ app/profile/page.js
@@ -215,7 +215,7 @@ export default function ProfilePage() {
           );
         };
 
-        logo.src = '/half-logo.svg';
+        logo.src = '/half-logo.png';
       };
 
       img.src =
@@ -437,7 +437,7 @@ UPI ID:- ${user.upiId}`;
                       />
 
                       <Image
-                        src="/half-logo.svg"
+                        src="/half-logo.png"
                         alt="UPay Logo"
                         width={35}
                         height={35}
